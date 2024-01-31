5b00ed59c1bd44bb6b4c7b9c6458ec86106be85d
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2024-01-31 21:35:40 +0900

    현식 merge

---
+++ app/src/main/java/com/example/jikgong/history_bottom_sheet_dialogFragment.kt
@@ -0,0 +1,25 @@
+package com.example.jikgong
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class history_bottom_sheet_dialogFragment : BottomSheetDialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history_bottom_sheet_dialog, container, false)
+    }
+
+}
